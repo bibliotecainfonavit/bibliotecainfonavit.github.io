2b50983ed3643ac65d9ef5a470d3f4a0734b8fc3
bibliotecainfonavit <131228044+bibliotecainfonavit@users.noreply.github.com>
2023-05-22 20:25:12 -0600

    Add files via upload

---
+++ opac-main/infonavit-menu.js
@@ -0,0 +1,43 @@
+const nav = {};
+
+nav.handleDropDown = () => {
+  $('.dropDown').on('click', function (e) {
+    e.stopPropagation();
+    $(this).next().toggle();
+    $('.nav__container__list--dropDown').not($(this).next()).hide();
+  });
+};
+
+nav.handleClickOffnav = () => {
+  $('html').click(function () {
+    $('.nav__container__list--dropDown').hide();
+  });
+};
+
+nav.handleHamburgerToggle = () => {
+  $('#hamburgerToggle').on('click', function () {
+    $('nav ul').toggle();
+  });
+
+  let x = false;
+  $("#hamburgerToggle").on("click", function () {
+    if (x) {
+      $(this).removeClass("active");
+      x = false;
+    } else {
+      $(this).addClass("active");
+      x = true;
+    }
+  });
+};
+
+nav.init = () => {
+  nav.handleClickOffnav();
+  nav.handleDropDown();
+  nav.handleHamburgerToggle();
+};
+
+//document ready
+$(function () {
+  nav.init();
+});
