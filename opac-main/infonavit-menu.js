eb7e8cc1a5c76723ccede15939fb20bc44233f04
arturguevara <126102635+arturguevara@users.noreply.github.com>
2023-08-15 13:23:26 -0600

    Update infonavit-menu.js

--- opac-main/infonavit-menu.js
+++ opac-main/infonavit-menu.js
@@ -1,55 +1,47 @@
-  const nav = {};
+const nav = {};
 
-  nav.handleDropDown = () => {
-    $('.dropDown').on('click', function (e) {
-      e.stopPropagation();
-      $(this).next().toggle();
-      $('.nav__container__list--dropDown').not($(this).next()).hide();
-    });
-  };
-
-  nav.handleClickOffnav = () => {
-    $('html').click(function () {
-      $('.nav__container__list--dropDown').hide();
-    });
-  };
-
-  nav.handleHamburgerToggle = () => {
-    $('nav ul').hide();
-    $('#hamburgerToggle').on('click', function () {
-      $('nav ul').toggle();
-    });
-
-    let x = false;
-    $("#hamburgerToggle").on("click", function () {
-      if (x) {
-        $(this).removeClass("active");
-        x = false;
-      } else {
-        $(this).addClass("active");
-        x = true;
-      }
-    });
-  };
-
-  nav.init = () => {
-    nav.handleClickOffnav();
-    nav.handleDropDown();
-    nav.handleHamburgerToggle();
-
-    // Cambiar color de texto para "Para niños y jóvenes"
-    $('.nav__container__list--subMenuParent > a:contains("Para niños y jóvenes")').hover(
-      function () {
-        $(this).css('color', 'pink');
-      },
-      function () {
-        $(this).css('color', 'white');
-      }
-    );
-  };
-
-  //document ready
-  $(document).ready(function () {
-    nav.init();
-    $('nav ul').hide();
+nav.handleDropDown = () => {
+  $('.dropDown').on('click', function (e) {
+    e.stopPropagation();
+    $(this).next().toggle();
+    $('.nav__container__list--dropDown').not($(this).next()).hide();
   });
+};
+
+nav.handleClickOffnav = () => {
+  $('html').click(function () {
+    $('.nav__container__list--dropDown').hide();
+  });
+};
+
+
+nav.handleHamburgerToggle = () => {
+  $('nav ul').hide();
+  $('#hamburgerToggle').on('click', function () {
+    $('nav ul').toggle();
+  });
+
+  let x = false;
+  $("#hamburgerToggle").on("click", function () {
+    if (x) {
+      $(this).removeClass("active");
+      x = false;
+    } else {
+      $(this).addClass("active");
+      x = true;
+    }
+  });
+};
+
+nav.init = () => {
+  nav.handleClickOffnav();
+  nav.handleDropDown();
+  nav.handleHamburgerToggle();
+};
+
+//document ready
+$(function () {
+  nav.init();
+});
+
+$('nav ul').hide();
