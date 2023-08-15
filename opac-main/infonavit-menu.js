7fbaada0b4b260afdae331bf75541d66669024cd
arturguevara <126102635+arturguevara@users.noreply.github.com>
2023-08-15 09:53:02 -0600

    Update infonavit-menu.js

--- opac-main/infonavit-menu.js
+++ opac-main/infonavit-menu.js
@@ -1,47 +1,55 @@
-const nav = {};
+    const nav = {};
 
-nav.handleDropDown = () => {
-  $('.dropDown').on('click', function (e) {
-    e.stopPropagation();
-    $(this).next().toggle();
-    $('.nav__container__list--dropDown').not($(this).next()).hide();
-  });
-};
+    nav.handleDropDown = () => {
+      $('.dropDown').on('click', function (e) {
+        e.stopPropagation();
+        $(this).next().toggle();
+        $('.nav__container__list--dropDown').not($(this).next()).hide();
+      });
+    };
 
-nav.handleClickOffnav = () => {
-  $('html').click(function () {
-    $('.nav__container__list--dropDown').hide();
-  });
-};
+    nav.handleClickOffnav = () => {
+      $('html').click(function () {
+        $('.nav__container__list--dropDown').hide();
+      });
+    };
 
+    nav.handleHamburgerToggle = () => {
+      $('nav ul').hide();
+      $('#hamburgerToggle').on('click', function () {
+        $('nav ul').toggle();
+      });
 
-nav.handleHamburgerToggle = () => {
-  $('nav ul').hide();
-  $('#hamburgerToggle').on('click', function () {
-    $('nav ul').toggle();
-  });
+      let x = false;
+      $("#hamburgerToggle").on("click", function () {
+        if (x) {
+          $(this).removeClass("active");
+          x = false;
+        } else {
+          $(this).addClass("active");
+          x = true;
+        }
+      });
+    };
 
-  let x = false;
-  $("#hamburgerToggle").on("click", function () {
-    if (x) {
-      $(this).removeClass("active");
-      x = false;
-    } else {
-      $(this).addClass("active");
-      x = true;
-    }
-  });
-};
+    nav.handleHoverColor = () => {
+      $('.nav__container__list--subMenuParent > a:contains("Para niñ")').hover(function() {
+        $(this).css('color', 'pink');
+      }, function() {
+        $(this).css('color', '');
+      });
+    };
 
-nav.init = () => {
-  nav.handleClickOffnav();
-  nav.handleDropDown();
-  nav.handleHamburgerToggle();
-};
+    nav.init = () => {
+      nav.handleClickOffnav();
+      nav.handleDropDown();
+      nav.handleHamburgerToggle();
+      nav.handleHoverColor();
+    };
 
-//document ready
-$(function () {
-  nav.init();
-});
+    //document ready
+    $(function () {
+      nav.init();
+    });
 
-$('nav ul').hide();
+    $('nav ul').hide();
